ebfef8e1afc422d383b31636c4cd21fb34d508a2
Lawrence Liang <[xlawrence.liangx@gmail.com]>
2022-03-17 22:24:07 -0700

    created FloatingActionButton, AddListActivity and AddItemActivity

---
+++ app/src/main/java/com/comp3717/itemtracker/AddItemActivity.java
@@ -0,0 +1,33 @@
+package com.comp3717.itemtracker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class AddItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_add, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/comp3717/itemtracker/MainActivity.java
+++ app/src/main/java/com/comp3717/itemtracker/MainActivity.java
@@ -1,13 +1,22 @@
 package com.comp3717.itemtracker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton add, addList, addItem;
+    Animation rotateOpen, rotateClose, fromBottom, toBottom;
+    private boolean clicked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +37,78 @@ public class MainActivity extends AppCompatActivity {
 
             return true;
         });
+
+
+        // creating animations for floating action buttons
+        add = findViewById(R.id.floatingActionButton_add);
+        addList = findViewById(R.id.floatingActionButton3_list);
+        addItem = findViewById(R.id.floatingActionButton2_item);
+
+        rotateOpen = AnimationUtils.loadAnimation(this, R.anim.rotate_open_animation);
+        rotateClose = AnimationUtils.loadAnimation(this, R.anim.rotate_close_animation);
+        fromBottom = AnimationUtils.loadAnimation(this, R.anim.from_botton_animation);
+        toBottom = AnimationUtils.loadAnimation(this, R.anim.to_bottom_animation);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onAddButtonClicked();
+            }
+        });
+
+        addList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        addItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddItemActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void onAddButtonClicked() {
+        setVisibility(clicked);
+        setAnimation(clicked);
+        setClickable(clicked);
+        clicked = !clicked;
+    }
+
+    private void setVisibility(Boolean clicked) {
+        if (!clicked) {
+            addList.setVisibility(View.VISIBLE);
+            addItem.setVisibility(View.VISIBLE);
+        } else {
+            addList.setVisibility(View.INVISIBLE);
+            addItem.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void setAnimation(Boolean clicked) {
+        if (!clicked) {
+            addItem.startAnimation(fromBottom);
+            addList.startAnimation(fromBottom);
+            add.startAnimation(rotateOpen);
+        } else {
+            addItem.startAnimation(toBottom);
+            addList.startAnimation(toBottom);
+            add.startAnimation(rotateClose);
+        }
+    }
+
+    private void setClickable(Boolean clicked) {
+        if (!clicked) {
+            addList.setClickable(true);
+            addItem.setClickable(true);
+        } else {
+            addList.setClickable(false);
+            addItem.setClickable(false);
+        }
     }
 }
