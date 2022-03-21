51e6aafec1b4315377e39f6a9de0a75e775277c4
Sunwoo Park <swparkaust@me.com>
2022-03-21 11:54:29 -0700

    Remove bottom navigation

--- app/src/main/java/com/comp3717/itemtracker/MainActivity.java
+++ app/src/main/java/com/comp3717/itemtracker/MainActivity.java
@@ -4,27 +4,16 @@ import static androidx.navigation.ui.NavigationUI.setupActionBarWithNavControlle
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 
-import com.comp3717.itemtracker.placeholder.PlaceholderContent;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,54 +28,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LinkedHashMap<String, PlaceholderContent.PlaceholderItem> LIST_TEMP = new LinkedHashMap<>();
-        lists_ref.addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@androidx.annotation.Nullable QuerySnapshot queryDocumentSnapshots, @androidx.annotation.Nullable FirebaseFirestoreException e) {
-                for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
-                    if (document.getData() != null) {
-                        PlaceholderContent.PlaceholderItem placeholderItem = new PlaceholderContent.PlaceholderItem(
-                                document.getId(),
-                                document.getData().get("Name").toString(),
-                                document.getData().get("Detail").toString(),
-                                (ArrayList<String>) document.getData().get("Lists"));
-                        if (!PlaceholderContent.LISTS.contains(placeholderItem)) {
-                            PlaceholderContent.LISTS.add(placeholderItem);
-                        }
-                        LIST_TEMP.put(document.getId(),placeholderItem);
-                    }
-                }
-                PlaceholderContent.LIST_MAP = LIST_TEMP;
-            }
-        });
-        LinkedHashMap<String, PlaceholderContent.PlaceholderItem> ITEM_TEMP = new LinkedHashMap<>();
-        items_ref.addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@androidx.annotation.Nullable QuerySnapshot queryDocumentSnapshots, @androidx.annotation.Nullable FirebaseFirestoreException e) {
-                for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
-                    if (document.getData() != null) {
-                        ITEM_TEMP.put(
-                                document.getId(), new PlaceholderContent.PlaceholderItem(document.getId(), document.getData().get("Name").toString())
-                        );
-                    }
-                }
-                PlaceholderContent.ITEM_MAP = ITEM_TEMP;
-            }
-        });
-        BottomNavigationView bottomNavigation = findViewById(R.id.bottomnavigationview_main);
-        bottomNavigation.setOnItemSelectedListener(item -> {
-            item.setChecked(true);
 
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            if (item.getItemId() == R.id.item_bottomnav_mylists) {
-                ft.replace(R.id.fragmentcontainerview_main, new MyListsFragment());
-            } else {
-                ft.replace(R.id.fragmentcontainerview_main, new SearchFragment());
-            }
-            ft.commit();
 
-            return true;
-        });
         // creating animations for floating action buttons
         add = findViewById(R.id.floatingactionbutton_main_add);
         addList = findViewById(R.id.floatingactionbutton_main_list);
